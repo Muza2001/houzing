7d9090aa85b5cd1afa8d577add3b7f57d36137c8
muza5 <muza5660@gmail.com>
2022-08-22 18:58:07 +0500

    first commit

---
+++ src/test/java/com/example/houzing/HouzingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.houzing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouzingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
